44f87eaa818a36a857aa5f2867de9522eb717aa2
ttl <talrasha007@gmail.com>
2017-05-18 10:10:18 +0800

    wait chain init before doing someting.

--- examples/info.js
+++ examples/info.js
@@ -3,6 +3,7 @@ const getChain = require('./getChain');
 (async function () {
   const chain = await getChain();
 
+  await chain.initialize();
   console.log('MSP members: ', await chain.queryMspMembers());
   console.log('Ledger info: ', await chain.queryInfo());
   console.log('Block 0 info: ', await chain.queryBlock(0));
