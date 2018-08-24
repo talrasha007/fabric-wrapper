ac3f21f25c5cec03d3c0dca89e7b8d55a37b787a
ttl <talrasha007@gmail.com>
2018-08-24 15:19:06 +0800

    node.js chain code.

---
+++ examples/chaincode/node/fcw_example/index.js
@@ -0,0 +1,52 @@
+const shim = require('fabric-shim');
+
+class Chaincode {
+
+  async Init(stub) {
+    const { params } = stub.getFunctionAndParameters();
+    if (params.length !== 1) {
+      return shim.error('Incorrect number of arguments.');
+    }
+
+    stub.putState('abc', Buffer.from(params[0]));
+    return shim.success();
+  }
+
+  async Invoke(stub) {
+    const ret = stub.getFunctionAndParameters();
+    console.error(ret);
+
+    let method = this[ret.fcn];
+    if (!method) {
+      console.error('no function of name:' + ret.fcn + ' found');
+      throw new Error('Received unknown function ' + ret.fcn + ' invocation');
+    }
+
+    try {
+      let payload = await method(stub, ret.params);
+      return shim.success(payload);
+    } catch (err) {
+      console.log(err);
+      return shim.error(err);
+    }
+  }
+
+  async write(stub, args) {
+    if (args.length !== 2) {
+      throw new Error('Incorrect number of arguments.');
+    }
+
+    await stub.putState(args[0], Buffer.from(args[1]));
+    return Buffer.from(`Set value of ${args[0]}`);
+  }
+
+  async read(stub, args) {
+    if (args.length !== 1) {
+      throw new Error('Incorrect number of arguments.');
+    }
+
+    return await stub.getState(args[0]);
+  }
+}
+
+shim.start(new Chaincode());
---
+++ examples/install-nodecc.js
@@ -0,0 +1,17 @@
+const network = require('./network');
+
+(async () => {
+  const client = await network.clientForOrderer();
+
+  const chaincodePath = __dirname + '/chaincode/node/fcw_example';
+
+  const resp = await client.installChaincode({
+    targets: network.getPeers(client),
+    chaincodePath,
+    chaincodeType: 'node',
+    chaincodeId: 'fcw_node',
+    chaincodeVersion: 'v0'
+  });
+
+  console.log(resp);
+})().catch(console.log);
