80496c6f54730c97ef5f6ae08c1e395b2e1f3fa6
Tal Rasha <talrasha007@gmail.com>
2018-08-26 19:05:04 +0800

    fix crypto gen.

--- examples/tls/generate-crypto.sh
+++ examples/tls/generate-crypto.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-alias cryptogen='docker run --rm\
-  -e FABRIC_CFG_PATH=/out\
-  -v $(pwd):/out\
-  hyperledger/fabric-tools:1.2.0 cryptogen'
+alias cryptogen='docker run --rm -v $(pwd):/out hyperledger/fabric-tools:1.2.0 cryptogen'
 
-cryptogen generate --output=/out/crypto-config
+cryptogen generate --config=/out/crypto.yaml --output=/out/crypto-config
